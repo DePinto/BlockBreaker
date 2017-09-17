830a3b033a7d039253dd3ef7da4c31616e4ce43c
DePinto <daniel.depinto@gmail.com>
2017-09-17 12:52:50 -0700

    Assets

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+	public LevelManager levelManager;
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log("Lose Collider Triggered");
+		levelManager.LoadLevel("Lose");
+	}
+}
